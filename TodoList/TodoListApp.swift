5b5574ff7fc1d0388411581e0d9c9cb8c63bb1d5
Dwistari <dwistari16@gmail.com>
2025-02-17 06:48:54 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dwistari on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
